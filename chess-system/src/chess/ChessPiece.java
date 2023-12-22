e71e639c286ee9925dfa3a6eba8fa2114e99dab1
Douglas Araujo <dougaraujor@gmail.com>
2023-12-21 22:10:19 -0300

    Possiveis movimentos de uma peca

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -3,17 +3,15 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
     private Color color;
-
-   
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
     }
-    
-     public Color getColor() {
+
+    public Color getColor() {
         return color;
     }
 }

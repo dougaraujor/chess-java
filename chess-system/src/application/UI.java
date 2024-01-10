6f8a73b1767d0e25f092e07dcc1c40d3d7603c11
Douglas Araujo <dougaraujor@gmail.com>
2024-01-10 16:17:49 -0300

    Logica CheckMate!

--- chess-system/src/application/UI.java
+++ chess-system/src/application/UI.java
@@ -56,9 +56,14 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turno: " + chessMatch.getTurn());
-        System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()) {
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
+        } else {
+            System.out.println("CHECKMATE!!!");
+            System.out.println("Vencedor: " + chessMatch.getCurrentPlayer());
         }
     }
 
